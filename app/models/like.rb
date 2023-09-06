d66d6c793254398c6bcb4d7bbe884b8f95a8573d
beteland123 <beteland23@gmail.com>
2023-09-07 01:05:11 +0300

    add the methods and the data for post , user, likes nd comment

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,15 @@
 class Like < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :post
+
+    after_create :update_post_likes_counter
+    before_destroy :update_post_likes_counter_before_destroy
+  
+    def update_post_likes_counter
+      post.increment!(:likescounter)
+    end
+  
+    def update_post_likes_counter_before_destroy
+      post.decrement!(:likescounter)
+    end
 end
