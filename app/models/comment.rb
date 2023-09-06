5447c3efdca18e1fdedc87417ff565f0ff0ae025
beteland123 <beteland23@gmail.com>
2023-09-07 01:26:06 +0300

    fix offense

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,13 +3,13 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_create :update_post_comments_counter
-  before_destroy :update_post_comments_counter_before_destroy
+  before_destroy :update_comments_before_destroy
 
   def update_post_comments_counter
     post.increment!(:commentscounter)
   end
 
-  def update_post_comments_counter_before_destroy
+  def update_comments_before_destroy
     post.decrement!(:commentscounter)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,13 +3,13 @@ class Like < ApplicationRecord
   belongs_to :post
 
   after_create :update_post_likes_counter
-  before_destroy :update_post_likes_counter_before_destroy
+  before_destroy :update_likes_before_destroy
 
   def update_post_likes_counter
     post.increment!(:likescounter)
   end
 
-  def update_post_likes_counter_before_destroy
+  def update_likes_before_destroy
     post.decrement!(:likescounter)
   end
 end
