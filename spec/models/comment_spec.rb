2347af1cd35b930aed6da57ab16b5a109b8e9aba
beteland123 <beteland23@gmail.com>
2023-09-07 23:19:57 +0300

    add test for likes and comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'associations' do
+    let(:user) {User.create(name: 'John',photo: 'http/lkk',bio: 'John bio',postcounter: 0)}
+
+    let(:post) {user.posts.create(title: 'my first post', likescounter: 0,commentscounter: 0)}
+    
+    subject {Comment.new(author: user,text: 'wow', post:)}
+    it 'belongs to an author' do
+      association = described_class.reflect_on_association(:author)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a post' do
+      association = described_class.reflect_on_association(:post)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe ' like counter' do
+    user = User.create(name: 'Lidiya', bio: 'Student from Eth')
+    post = Post.create(author: user, title: 'SW ', text: 'Happy coding!')
+    comment = Comment.new(author: user,text: 'wow', post:)
+
+    it 'should increase comments_counter after creatation ' do
+      expect { comment.save }.to change { post.commentscounter}.by(1)
+    end
+
+    it 'should decrease likes_counter after destroy' do
+      expect { comment.destroy }.to change { post.reload.commentscounter }.by(-1)
+    end
+  end
+ 
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'associations' do
+    let(:user) {User.create(name: 'John',photo: 'http/lkk',bio: 'John bio',postcounter: 0)}
+
+    let(:post) {user.posts.create(title: 'my first post', likescounter: 0,commentscounter: 0)}
+    
+    subject {Like.new(author: user, post:)}
+    it 'belongs to an author' do
+      association = described_class.reflect_on_association(:author)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a post' do
+      association = described_class.reflect_on_association(:post)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe 'like counter' do
+    user = User.create(name: 'Lidiya', bio: 'Student from Eth')
+    post = Post.create(author: user, title: 'SW ', text: 'Happy coding!')
+    like = Like.new(author: user, post:)
+
+    it 'should increase likes_counter after creatation ' do
+      expect { like.save }.to change { post.likescounter }.by(1)
+    end
+
+    it 'should decrease likes_counter after destroy' do
+      expect { like.destroy }.to change { post.reload.likescounter }.by(-1)
+    end
+  end
+ 
 end
