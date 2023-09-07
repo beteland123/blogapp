b9860af32e8c8d0cd4a5e647c62042a31678aa07
beteland123 <beteland23@gmail.com>
2023-09-07 23:08:34 +0300

    add validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :commentscounter, numericality: { only_integer: true, greater_than: 0 }
-  validates :likescounter, numericality: { only_integer: true, greater_than: 0 }
+  validates :commentscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likescounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_create :increment_user_posts_count
   before_destroy :update_post_counter_before_destroy
