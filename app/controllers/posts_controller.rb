0bbc596312ed4fd2057c149074b2613180e9918a
beteland123 <beteland23@gmail.com>
2023-09-11 19:02:32 +0300

    fix linter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,17 @@
 class PostsController < ApplicationController
-    before_action :set_user
-    def index
-        @posts = @user.posts.includes(:comments)
-      end
-    
-      def show
-        @post = @user.posts.find(params[:id])
-        @comments = @post.comments
-      end
-      
-      private
+  before_action :set_user
+  def index
+    @posts = @user.posts.includes(:comments)
+  end
 
-     def set_user
-       @user = User.find(params[:user_id])
-    end
+  def show
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+  end
 
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
