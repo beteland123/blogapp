bb59a40e6b5764894dde3a1247b33ff0bc97d71b
beteland123 <beteland23@gmail.com>
2023-09-13 00:41:46 +0300

    add button with the eventlinster

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', function() {
+    const showAllBtn = document.getElementById('show-all-btn');
+    const remainingPosts = document.getElementById('remaining-posts');
+  
+    showAllBtn.addEventListener('click', function() {
+      remainingPosts.classList.toggle('hidden');
+      showAllBtn.classList.toggle('hidden');
+    });
+  });
