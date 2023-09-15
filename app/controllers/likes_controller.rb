957c532f13efb9159293f384d08e02f7843cf05f
beteland123 <beteland23@gmail.com>
2023-09-15 03:47:09 +0300

    fix offense

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
-    def create
-      @post = Post.find(params[:post_id])
-      @like = @post.likes.build(author: current_user)
-      
-      if @like.save
-        flash[:success] = "Like added successfully"
-      else
-        flash[:error] = "Error: Like could not be added"
-      end
-      
-      redirect_to user_post_path(@post.author, @post)
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.build(author: current_user)
+
+    if @like.save
+      flash[:success] = 'Like added successfully'
+    else
+      flash[:error] = 'Error: Like could not be added'
     end
+
+    redirect_to user_post_path(@post.author, @post)
   end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,30 +8,33 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
     @comments = @post.comments
   end
+
   def new
     @post = Post.new
     respond_to do |format|
       format.html { render :new, locals: { post: @post } }
     end
   end
+
   def create
     @post = current_user.posts.build(post_params)
     respond_to do |format|
       format.html do
         if @post.save
-          flash[:success] = "Post saved successfully"
+          flash[:success] = 'Post saved successfully'
           redirect_to user_posts_path(current_user)
         else
           # error message
-          flash.now[:error] = "Error: Post could not be saved"
+          flash.now[:error] = 'Error: Post could not be saved'
           # render new
           render :new, locals: { post: }
         end
       end
     end
   end
+
   private
-  
+
   def post_params
     params.require(:post).permit(:title, :text)
   end
