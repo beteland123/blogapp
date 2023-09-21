d6f34ee68717edd5a323c2b9bd1b1216174bcf49
beteland123 <beteland23@gmail.com>
2023-09-21 17:02:40 +0300

    genrate ablity and implement role

---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+    user ||= User.new
+
+    if user&.admin?
+      can :manage, :all
+    else 
+      can :read, :all
+      can :create, [Post, Comment]
+      can :destroy, Post, author_id: user.id
+      can :destroy, Comment, author_id: user.id
+    end
+  end
+end
